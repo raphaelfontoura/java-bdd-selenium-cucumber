f4072723024549cce6a2e0e3c1ebd907f7dd1408
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-11-27 13:18:15 -0300

    Implement cucumber steps and defined feature in classpath resources

---
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -0,0 +1,26 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PropondoLanceSteps {
+
+    @Given("Dado um lance valido")
+    public void dado_um_lance_valido() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Quanto propoe o lance")
+    public void quanto_propoe_o_lance() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Entao o lanche eh aceito")
+    public void entao_o_lanche_eh_aceito() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/resources/features/propondo_lances.feature
@@ -0,0 +1,6 @@
+Feature: Propondo lances
+
+Scenario: Propondo um unico lance valido
+    Given Dado um lance valido
+    When Quanto propoe o lance
+    Then Entao o lanche eh aceito
